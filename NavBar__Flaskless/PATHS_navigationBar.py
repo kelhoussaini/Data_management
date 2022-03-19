cc851fcbc35f3f6ce7c66f9ac9d72c7c518f2d32
Kenza EL HOUSSAINI <kenzaelhoussaini@gmail.com>
2022-03-19 10:04:05 +0100

    Navigation Bar components

---
+++ NavBar__Flaskless/PATHS_navigationBar.py
@@ -0,0 +1,21 @@
+NAVBAR_PATHS = {
+    'HOME':'home',
+    'ABOUT': 'about',
+    'DATA MANAGEMENT': 'management',
+    'STATISTICS': 'statistics', 
+    'ANALYSIS': 'analysis',
+    'VISUALIZATION': 'visualization'
+}
+
+SETTINGS = {
+    'OPTIONS':'options',
+    'CONFIGURATION':'configuration'
+}
+
+
+ANALYSIS_DROPDOWN = {
+    'ANALYSIS':'analysis',
+    'STATISTICS':'statistics'
+}
+
+
